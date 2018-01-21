edb7c65e3aa6ed177720e23a2053ccb67a33db95
simplex <zhuzhongshu@foxmail.com>
2018-01-22 01:10:46 +0800

    ycmd设置强制使用语法补全，不然没法补全文件中的变量

--- core/el-get-init-files/init-ycmd.el
+++ core/el-get-init-files/init-ycmd.el
@@ -12,9 +12,11 @@
 
 ;; ycmd-extra-conf-whitelist is a LIST
 ;; (set-variable 'ycmd-extra-conf-whitelist '( "C:/Users/zhuzh/AppData/Roaming/.emacs.d/ycmdcpp/ycm"))
-(set-variable 'ycmd-extra-conf-whitelist `(,(concat my-home-dir "/.emacs.d/ycmd/cpp/ycm")))
+;; (set-variable 'ycmd-extra-conf-whitelist `(,(concat my-home-dir "/.emacs.d/ycmd/cpp/ycm")))
 
 ;; (setq ycmd-python-binary-path  (concat python-executable-path "python3"))
+
+(setq ycmd-force-semantic-completion t)
 
 (global-ycmd-mode)
 
