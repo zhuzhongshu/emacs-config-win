47c31d0f11d7fd30b184f499be21721b593ae0d7
simplex <zhuzhongshu@foxmail.com>
2018-08-08 08:34:38 +0800

    增加format-all插件

---
+++ core/el-get-init-files/init-format-all.el
@@ -0,0 +1,8 @@
+(require 'format-all)
+
+(global-set-key [f10] 'format-all-buffer)
+
+;; (add-hook 'c-mode-common-hook 'format-all-mode)
+;; (add-hook 'python-mode-hook 'format-all-mode)
+;; (add-hook 'json-mode-hook 'format-all-mode)
+;; (add-hook 'markdown-mode-hook 'format-all-mode)
