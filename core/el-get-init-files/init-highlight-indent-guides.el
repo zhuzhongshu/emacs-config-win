f0cc6e9736f40e801496bae31b40ccbe95181797
simplex <zhuzhongshu@foxmail.com>
2018-09-02 15:46:11 +0800

    增加highlight-indent-guides配置

---
+++ core/el-get-init-files/init-highlight-indent-guides.el
@@ -0,0 +1,12 @@
+
+(add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
+
+
+(setq highlight-indent-guides-method 'character)
+
+(setq highlight-indent-guides-responsive 'top)
+
+(setq highlight-indent-guides-delay 0.5)
+
+;;默认的是9474，对应中文字符|，比较宽，可能造成对齐不准，这里改成英文的|
+(setq highlight-indent-guides-character 124)
