33dd943de321eba7c9a1ec79ed26f58f5c8e1728
simplex <zhuzhongshu@foxmail.com>
2018-09-02 15:49:11 +0800

    更改错误和警告的提示标记

--- core/el-get-init-files/init-flycheck-posframe.el
+++ core/el-get-init-files/init-flycheck-posframe.el
@@ -1,8 +1,5 @@
 (with-eval-after-load 'flycheck
   (require 'flycheck-posframe)
   (add-hook 'flycheck-mode-hook #'flycheck-posframe-mode))
-(setq flycheck-posframe-warning-prefix "\u26a0 ")
-(setq flycheck-posframe-error-prefix "✕ ")
-
-
-
+(setq flycheck-posframe-warning-prefix "⁇ ")
+(setq flycheck-posframe-error-prefix "✗ ")
